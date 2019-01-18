56fc5dea1fc18dc3553fa87677381cc35acd9c6b
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2019-01-18 16:48:37 +0100

    ci compile stuff

---
+++ examples/_internal_compile_test/_internal_compile_test.ino
@@ -0,0 +1,118 @@
+/*
+    WARNING!!!
+    This Example is only used for internal testing of the library.
+    It don't represent a propper usecase for this library.
+
+    So please ignore this file!
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "SRF08.h"
+
+SRF08 SRF08;
+
+void setup() {
+    SRF08.init();
+    SRF08.startRangeReading();
+    SRF08.startRangeReadingUNSAFE();
+    SRF08.readRange();
+    SRF08.readRangeUNSAVE();
+    SRF08.checkIfReadyForReading();
+    SRF08.getDistance();
+    SRF08.getTimeOfLastReading();
+    SRF08.get_version();
+}
+
+void loop() {
+}
