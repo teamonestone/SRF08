9bd851394dbafef9ff7dc4994449a541e683fb5d
Travis CI <travis@travis-ci.org>
2019-10-30 21:45:18 +0000

    Deploy code docs to GitHub Pages Travis build: 31
    Commit: a7a5365d4c7b03756692f29d5b207460f0af211f

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['get_5fversion',['get_version',['../class_s_r_f08.html#ae975cfa4f50f934dedae7710712bac43',1,'SRF08']]],
-  ['getdistance',['getDistance',['../class_s_r_f08.html#a579fed0fd26f744274aae9e6fc5445dc',1,'SRF08']]],
-  ['gettimeoflastreading',['getTimeOfLastReading',['../class_s_r_f08.html#aaa9f28f5942fab530e0b2e0e2db1f950',1,'SRF08']]]
+  ['checkifreadyforreading',['checkIfReadyForReading',['../class_s_r_f08.html#af4230fb082363fd5ec354ee05386774c',1,'SRF08']]]
 ];
--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['std_5fsrf08_5faddress',['STD_SRF08_ADDRESS',['../_s_r_f08_8h.html#aed4d7670f856a55b3d70d2d4e3353d03',1,'SRF08.h']]]
+  ['_5fsrf08_5flib_5fversion',['_SRF08_LIB_VERSION',['../_s_r_f08_8h.html#a29a1a13fb4aef59348a95540589f938b',1,'SRF08.h']]],
+  ['_5fsrf08_5fstd_5faddress',['_SRF08_STD_ADDRESS',['../_s_r_f08_8h.html#aba37514d84620d2537f5daf8da8464e0',1,'SRF08.h']]]
 ];
