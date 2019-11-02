e667c122970905365985d359cebaff74d1b6eaba
Travis CI <travis@travis-ci.org>
2019-11-02 16:25:09 +0000

    Deploy code docs to GitHub Pages Travis build: 35
    Commit: 337f702a9d8073231e8e7a7fc0d2c0e5755f21f1

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['get_5fversion',['get_version',['../class_s_r_f08.html#ae975cfa4f50f934dedae7710712bac43',1,'SRF08']]],
   ['getdistance',['getDistance',['../class_s_r_f08.html#a579fed0fd26f744274aae9e6fc5445dc',1,'SRF08']]],
-  ['gettimeoflastreading',['getTimeOfLastReading',['../class_s_r_f08.html#aaa9f28f5942fab530e0b2e0e2db1f950',1,'SRF08']]]
+  ['gettimeoflastreading',['getTimeOfLastReading',['../class_s_r_f08.html#aa85bf75a55b5e77875a275b8cebe5abf',1,'SRF08']]]
 ];
